1ebe6d462606db52b3f7a378c2ce1dd2a71b6dd9
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-21 14:59:47 -0300

    .

--- Curso Javascript/000 - Objet/002 - Propiedade/index.js
+++ Curso Javascript/000 - Objet/002 - Propiedade/index.js
@@ -20,7 +20,7 @@ function Tenis(tamanho, estoque, preco) {
     };
 }
 
-console.log(Tenis(35, true, "R$ 1.250,68"));
+console.log(Tenis(35, true, "R$ 250,68"));
  
 
 
@@ -31,7 +31,7 @@ console.log(Tenis(35, true, "R$ 1.250,68"));
 
 const tamanho = 40;
 const estoque = true;
-const preco = "R$ 1.250,68";
+const preco = "R$ 2.250,68";
 
 const Tenis2 = {
     tamanho,
@@ -42,12 +42,3 @@ const Tenis2 = {
 console.log(Tenis2)
 
 
-//------------Com Metodos--------------
-
-const Tenis3 = {  
-    getTamanho(){
-        return 35
-    },
-};
-
-console.log( Tenis3())
