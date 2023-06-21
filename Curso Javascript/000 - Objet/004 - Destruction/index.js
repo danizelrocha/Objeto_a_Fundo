9bf4549d5721b3e5a945be5a1c986917dd275c23
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-21 15:20:00 -0300

    parcial

---
+++ Curso Javascript/000 - Objet/004 - Destruction/index.js
@@ -0,0 +1,9 @@
+const Tenis = {
+    tamanho: 45,
+    estoque: true,
+    marcas: [{nome: "Adidas"}, { nome: "Nike"}],
+};
+
+const { tamanho, estoque, marcas,} = Tenis;
+
+console.log(tamanho, estoque, marcas);
