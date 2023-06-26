ca231cc92e47b2bc5940bce9b8d8ccbe41d801a4
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-26 08:38:06 -0300

    Destructuring

--- Curso Javascript/000 - Objet/004 - Destruction/index.js
+++ Curso Javascript/000 - Objet/004 - Destruction/index.js
@@ -2,8 +2,46 @@ const Tenis = {
     tamanho: 45,
     estoque: true,
     marcas: [{nome: "Adidas"}, { nome: "Nike"}],
+    secret: 123456,
+    n: 5,
+    link: { a:"a é igual A", b: { c:"c é igual C"} },
 };
 
-const { tamanho, estoque, marcas,} = Tenis;
+//const { tamanho, estoque, marcas,} = Tenis;
+//console.log(tamanho, estoque, marcas);
 
-console.log(tamanho, estoque, marcas);
+/* const {secret: randomNumber, n: avaliacoes } = Tenis;
+//console.log(secret);
+console.log(randomNumber);
+console.log(avaliacoes); */
+
+
+
+//  Para charma a
+/* const {
+    link: { a } ,
+} =Tenis;
+
+console.log(a); */
+
+
+// Para chamar c
+
+/* const {
+    link: { 
+        b: { c },
+    } ,
+} =Tenis;
+
+console.log(c); */
+
+// Para chamar "A" e "C"
+
+const {
+    link: { 
+        a,
+        b: { c },
+    } ,
+} =Tenis;
+
+console.log(a,c);
