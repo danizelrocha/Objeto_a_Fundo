8f24d80f187812449ef09cb96d2ca5f4f8a744bc
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-27 10:01:35 -0300

    Elementos no arry

---
+++ Curso Javascript/ARRAY A FUNDO/002 - Indeces e elementos/index.js
@@ -0,0 +1,13 @@
+// Nome está como objeto e fixa sozinho na tabela.
+
+/* const arr = [1, 2, 3, 4, 5, 6, true, "Daniel", { nome:"É pai da Fernanda, Laura e Manuela"}];
+
+console.table(arr);  */
+
+// Agora seu objeto virou uma string
+
+const arr = [1, 2, 3, 4, 5, 6, true, "Daniel", " É pai da", "Fernanda", "Laura", "e", "Manuela"];
+
+console.table(arr); 
+console.log(arr[7]);
+console.log(arr.length); 
