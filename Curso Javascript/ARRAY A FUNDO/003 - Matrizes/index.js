753d0507a89c10e06e60f74d70adb16dd9dcb630
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-28 07:49:56 -0300

    matriz

--- Curso Javascript/ARRAY A FUNDO/003 - Matrizes/index.js
+++ Curso Javascript/ARRAY A FUNDO/003 - Matrizes/index.js
@@ -10,7 +10,7 @@ console.log(arr[2][0]);// [2] é a eixo "X" e o [0] é o eixo "Y" */
 
 
 
-const arr = [
+/* const arr = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
@@ -22,4 +22,27 @@ const arr = [
 ];
 
 console.table(arr);
-console.log(arr[3][2][1]);// [3] é a eixo "X" e o [2] é o eixo "X" e o [1] é o eixo "y" Aqui passamo por todo primeiro arry para entrar no segundo
+console.log(arr[3][2][1]);// [3] é a eixo "X" e o [2] é o eixo "X" e o [1] é o eixo "y"
+                          // Aqui passamo por todo primeiro arry para entrar no segundo */
+
+
+
+
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ],
+    ],
+];
+
+console.table(arr);
+console.log(arr[3][2][1]);
