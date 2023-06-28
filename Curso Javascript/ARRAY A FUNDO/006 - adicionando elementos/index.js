9b38a21e7533048fbf3ecaa23dce426fe0b8bed9
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-28 08:53:53 -0300

    Adição de elementos

---
+++ Curso Javascript/ARRAY A FUNDO/006 - adicionando elementos/index.js
@@ -0,0 +1,45 @@
+// unshift = add tem no inico da array
+// push = add item no final do array
+
+
+
+
+// forma silmples mas impraticavel emum arry com varias matrizes
+/* let arr = [1, 2, 3, 4, 5,];
+arr[5] = 6;
+console.log(arr); */
+
+
+
+
+// metodo de adicionar eficas no fim da arry usando o - PUSH
+/* let arr = [1, 2, 3, 4, 5,];
+arr.push(6);
+console.log(arr); */
+
+
+
+// metodo usado para colocar o elemento no inicio da arry
+/* let arr = [1, 2, 3, 4, 5,];
+arr.unshift(6);
+console.log(arr); */
+
+
+
+// forma de adicionar nu inicio e fim da arry
+/* let arr = [1, 2, 3, 4, 5,];
+arr.unshift(0);
+arr.push(6);
+console.log(arr); */
+
+
+
+// forma de incerir variadas vezes elementos tanto no inicio como no final da arry
+let arr = [1, 2, 3, 4, 5,];
+arr.unshift(0);
+arr.unshift(-1);
+arr.unshift(-2);
+arr.push(6);
+arr.push(7);
+arr.push(8);
+console.log(arr);
