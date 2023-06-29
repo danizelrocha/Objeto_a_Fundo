5a816d5049858e6fa2041e20125341a48ea810a3
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-29 09:28:14 -0300

    Foreach

--- Curso Javascript/ARRAY A FUNDO/012 - Foreach/index.js
+++ Curso Javascript/ARRAY A FUNDO/012 - Foreach/index.js
@@ -1,3 +1,5 @@
+//----------------------------Forech-----------------------
+
 const myArrayObj = [
     {
         nome: "Daniel",
@@ -24,3 +26,35 @@ const myArrayObj = [
         sobreNome: "Rocha",
     },
 ];
+/* 
+// element => calbackFunction ou no lugar de element podemos usar item.
+myArrayObj.forEach(element => {
+    console.log(element);
+});
+ */
+
+
+
+// element => calbackFunction ou no lugar de element podemos usar item.
+// Utilizando index ele retorna a posição de cada objeto
+/* myArrayObj.forEach(( index, element) => {
+    console.log(index, element);
+}); */
+
+
+// Retornando mensagens
+/* myArrayObj.forEach((element, index) => {
+    if(element.nome === "Manuela"){
+     console.log(" A Manu está esperando o Sushi");   
+    }
+    console.log(index, element.nome);
+}); */
+
+
+// Retornando mensagens utilizando return forma adequada e retornar .
+myArrayObj.forEach((element, index) => {
+    if(element.nome === "Manuela"){
+      return console.log(" A Manu está esperando o Sushi");   
+    }
+    console.log(index, element.nome);
+});
