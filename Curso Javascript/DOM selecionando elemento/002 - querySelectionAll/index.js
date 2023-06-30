786689e51730083e1fca839562745238ea7ec936
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-30 17:21:41 -0300

    All

---
+++ Curso Javascript/DOM selecionando elemento/002 - querySelectionAll/index.js
@@ -0,0 +1,10 @@
+const p = document.querySelectorAll("p");
+
+p.forEach((element, index) => {
+    if(index ===0){
+        element.style.background ="blue";
+    }
+    if( index ===1){
+        element.style.background = "pink";
+    }
+});
