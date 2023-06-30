7e4b49fb97614dcccc1188f4e2a3ec17758f7d27
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-30 08:07:56 -0300

    Reduce

--- Curso Javascript/ARRAY A FUNDO/018 - Reduce/index.js
+++ Curso Javascript/ARRAY A FUNDO/018 - Reduce/index.js
@@ -1,39 +1,44 @@
 
-
+// A idéia é pegar todos os valores da array e condensa-lo em um só
 const pedidos = [
     {
         id: 420,
         nome: "Daniel",
         alimento: "X Bacon",
-        bebida: "Suco da Laranja"
+        bebida: "Suco da Laranja",
+        preco: 52,
     },
     {
         id: 4152,
         nome: "Laura",
         alimento: "Sanduiche Natural",
-        bebida: "Chá"
+        bebida: "Chá",
+        preco: 32,
     },
     {
         id: 29,
         nome: "Manuela",
         alimento: "Sushi",
-        bebida: "Suco da Morango"
+        bebida: "Suco da Morango",
+        preco: 48,
     },
     {
         id: 33,
         nome: "Fernada",
         alimento: "X Salada",
-        bebida: "Chá"
+        bebida: "Chá",
+        preco: 36,
     },
     {
         id: 55,
         nome: "Tiago",
         alimento: "Pizza Portuguesa",
-        bebida: "Coca-cola de 2 Litros"
+        bebida: "Coca-cola de 2 Litros",
+        preco: 98,
     },
 ];
 
-const temCha = pedidos.every( (element) => {
-    return element.bebida ==="Chá";    
-});
-console.log(temCha);
+const balancete = pedidos.reduce( (total, element) => {
+    return total + element.preco ;    
+}, 0);// O zero aqui representa o ponto de partida da soma do total. Já que precissa iniciar com um valor que não sabemos 
+console.log(balancete);
