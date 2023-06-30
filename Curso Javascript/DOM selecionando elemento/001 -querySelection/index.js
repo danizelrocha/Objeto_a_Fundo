7acf98e718593522fb046702a030543ab734b259
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-30 17:10:20 -0300

    Query manipulando o DOM

--- Curso Javascript/DOM selecionando elemento/001 -querySelection/index.js
+++ Curso Javascript/DOM selecionando elemento/001 -querySelection/index.js
@@ -7,11 +7,18 @@ const h1 = document.querySelector("h1"); // acessamdo por TAG HTML
 h1.style.background = "rgb(18, 208, 47)";
 h1.style.textAlign = "center";
 
-const classP = document.querySelector(".paragrafo strong"); //Aqui "paragrfo" é o pai e "stroge" é o filho "child" sempre dar um espaço entre eles
-
+const classP = document.querySelector(".paragrafo"); //Aqui "paragrfo" é o pai e "stroge" é o filho "child" sempre dar um espaço entre eles
 const idP = document.querySelector("#paragrafo");
 
 console.log(h1);
-console.log(classP.querySelector("strong"));// ou você pode usar desta forma tambem que foi referenciado na linha 10 um ou outro
+console.log(classP);
 console.log(idP);
+
+let timer = 0;
+const title = document.querySelector("title");
+
+setInterval(() =>{
+    title.innerText = timer;
+    timer++;
+}, 1000);
 
