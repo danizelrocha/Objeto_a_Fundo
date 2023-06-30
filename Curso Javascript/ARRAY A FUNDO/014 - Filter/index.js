8ecc09b3fc7a8caf20d36ccadbada705b46661f1
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-30 07:31:43 -0300

    Filter

---
+++ Curso Javascript/ARRAY A FUNDO/014 - Filter/index.js
@@ -0,0 +1,39 @@
+const pedidos = [
+    {
+        id: 420,
+        nome: "Daniel",
+        alimento: "X Bacon",
+        bebida: "Suco da Laranja"
+    },
+    {
+        id: 4152,
+        nome: "Laura",
+        alimento: "Sanduiche Natural",
+        bebida: "Chá"
+    },
+    {
+        id: 29,
+        nome: "Manuela",
+        alimento: "Sushi",
+        bebida: "Suco da Morango"
+    },
+    {
+        id: 33,
+        nome: "Fernada",
+        alimento: "X Salada",
+        bebida: "Chá"
+    },
+    {
+        id: 55,
+        nome: "Tiago",
+        alimento: "Pizza Portuguesa",
+        bebida: "Coca-cola de 2 Litros"
+    },
+];
+
+const filterCha = pedidos.filter((element, index) => {
+        return element.bebida === "Chá";
+    });
+
+console.log(1, pedidos);
+console.log(2, filterCha);
