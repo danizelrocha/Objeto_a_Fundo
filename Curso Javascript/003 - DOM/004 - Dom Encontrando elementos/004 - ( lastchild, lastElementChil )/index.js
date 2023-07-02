28a93438a8193d427ee5d778840ecc98b125a596
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-02 09:59:40 -0300

    Last Child

---
+++ Curso Javascript/003 - DOM/004 - Dom Encontrando elementos/004 - ( lastchild, lastElementChil )/index.js
@@ -0,0 +1,16 @@
+// Pega o container
+const container = document.querySelector(".container");
+
+console.log(container.lastChild);
+console.log(container.lastElementChild);
+
+
+// Pega o paragrafo
+const p = document.querySelector(".paragrafo");
+
+console.log(p.lastChild);
+console.log(p.lastElementChild);
+
+
+
+
