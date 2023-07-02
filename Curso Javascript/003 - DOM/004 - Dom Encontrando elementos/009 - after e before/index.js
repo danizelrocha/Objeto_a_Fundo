2d64158b5ad6a6dd4183af9f49dce75f349931be
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-02 14:45:03 -0300

    after before

---
+++ Curso Javascript/003 - DOM/004 - Dom Encontrando elementos/009 - after e before/index.js
@@ -0,0 +1,10 @@
+
+/* const container = document.querySelector(".container");
+const newP = document.createElement("p");
+newP.innerText = "Olá Daniel";
+container.before(newP); */
+ 
+ const container = document.querySelector(".container");
+const newP = document.createElement("p");
+newP.innerText = "Olá Daniel";
+container.after(newP); 
