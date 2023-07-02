c68eee196d1bc3ca912b150d9ee07a89ec76a8a4
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-02 09:53:02 -0300

    Filho list

---
+++ Curso Javascript/003 - DOM/004 - Dom Encontrando elementos/003 - Filho ( first children, Elementchild )/index.js
@@ -0,0 +1,15 @@
+// Pegando elementos do container
+
+const container = document.querySelector(".container");
+
+console.log(container.firstChild);// pega o primeiro elemento filho que é text um pedaço de espaço
+console.log(container.firstElementChild);// pega o primeiro elemento filho que é texto do html " Encontrando Elemento", agora sim
+
+
+
+//Pegando elemento do paragrafo
+
+const p = document.querySelector(".paragrafo");
+
+console.log(p.firstChild);//Pega o texto em loren if inteiro
+console.log(p.firstElementChild);//Pegou o Strong
