e55a3caae5a370e48e6d3e05c2ae200912013626
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-02 14:00:09 -0300

    previousSibling

---
+++ Curso Javascript/003 - DOM/004 - Dom Encontrando elementos/006 - Irmãos ( previousSibling, previuosElementSiblig )/index.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector("h1"); // Pega o anterior irmão do h1
+console.log(h1.previousSibling);// pega tudo - texto espaçamento...
+console.log(h1.previousElementSibling);//pega elemento
+
+
+const p = document.querySelector(".paragrafo");// pega o anterior irmão  do p.
+console.log(p.previousSibling); // pega tudo - texto espaçamento...
+console.log(p.previousElementSibling);//pega elemento
