7aa61bbec536039b06d653fcc1d259f151ba7f9d
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-02 14:10:20 -0300

    append

---
+++ Curso Javascript/003 - DOM/004 - Dom Encontrando elementos/007 - Append e appendChild/index.js
@@ -0,0 +1,11 @@
+
+// Adiciona elementos com append
+const container = document.querySelector(".container");
+container.append("Olá Daniel");
+
+
+
+//Adicionando elemenytos com appendChild
+const newDiv = document.createElement("div");
+newDiv.innerText = "Olá Manu";
+container.appendChild(newDiv);
