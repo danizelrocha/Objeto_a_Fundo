7f73c10a35303abe8a5b97f0b98ba5c9aad8ee04
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-02 13:54:55 -0300

    nextSibling

---
+++ Curso Javascript/003 - DOM/004 - Dom Encontrando elementos/005 - Irmãos (nextSibling, nextElementSiblig )/index.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("h1"); // Pega o proximo irmão do h1
+console.log(h1.nextSibling);
+console.log(h1.nextElementSibling);
+
+
+
+
+const p = document.querySelector(".paragrafo");// pega o proximo irmão do p aqui é o id.
+console.log(p.nextSibling);
+console.log(p.nextElementSibling);
