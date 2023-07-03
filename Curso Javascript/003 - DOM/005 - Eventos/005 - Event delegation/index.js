305f85f40e25a65e71895ca8b9525a8bb0c7fdb0
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-03 09:03:03 -0300

    Event delegation

---
+++ Curso Javascript/003 - DOM/005 - Eventos/005 - Event delegation/index.js
@@ -0,0 +1,44 @@
+// Errado de se fazer ruim para manutenção e consumo de memória
+
+/* const home = document.querySelector(".home");
+home.addEventListener("click", () => {
+    console.log("Home");
+});
+
+const sobre = document.querySelector(".sobre");
+sobre.addEventListener("click", () => {
+    console.log("Sobre");
+});
+
+const contato = document.querySelector(".contato");
+contato.addEventListener("click", () => {
+    console.log("Contato");
+}); */
+
+
+//--------------------------------------------------------
+
+
+// Como deve ser feito, melhorando a performance e manutenção
+
+const menu = document.querySelector("#menu");
+
+menu.addEventListener("click", (event) => {
+    const { target } = event;
+    const body = document.querySelector('body')
+
+    switch (target.getAttribute("class")) {
+        case "home":
+            body.style.background = "red"
+            break;
+
+        case "sobre":
+            body.style.background = "blue"
+            break;
+
+        case "contato":
+            body.style.background = "pink"
+            break;
+    }
+
+});
