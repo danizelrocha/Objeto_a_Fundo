8941090737e6e345817960f60abfcd11febffc5c
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-04 08:30:05 -0300

    Manipulação de Ckeck box

--- Curso Javascript/003 - DOM/006 - Formularios/003 - Manipulando Input/index.js
+++ Curso Javascript/003 - DOM/006 - Formularios/003 - Manipulando Input/index.js
@@ -1,5 +1,24 @@
 "use strict";
 
+
+//Com Submit
+/* const form = document.forms.namedItem("registration");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = form.name.value;
+
+  const newDivElement = document.createElement("div");
+  newDivElement.innerText = name;
+
+  form.nextElementSibling.remove();
+  form.after(newDivElement);
+
+}); */
+
+
+//    Com input
 const form = document.forms.namedItem("registration");
 
 form.addEventListener("input", (event) => {
@@ -12,4 +31,4 @@ form.addEventListener("input", (event) => {
 
   form.nextElementSibling.remove();
   form.after(newDivElement);
-});
+}); 
